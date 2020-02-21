0a22d2dd5f3f96c22ef1e3b906dc717b682d9130
dez.osk@gmail.com <dez.osk@gmail.com>
2020-02-21 23:28:37 +0000

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <nav-header @pageWasChanged="currentPage=$event"></nav-header>
+        <transition name="fade" mode="out-in">
+        <component :is="currentPage"></component>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import CardFront from './components/card/CardFront.vue'
+import CardInsideLeft from './components/card/CardInsideLeft.vue'
+import CardInsideRight from './components/card/CardInsideRight.vue'
+import CardBack from './components/card/CardBack.vue'
+
+export default {
+  data: function () {
+    return {
+      currentPage: 'cardFront'
+    }
+  },
+  components: {
+    navHeader: Header,
+    cardFront: CardFront,
+    cardInsideLeft: CardInsideLeft,
+    cardInsideRight: CardInsideRight,
+    cardBack: CardBack
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    color: #333
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: translateX(-2em);
+  } 
+
+  .fade-enter-active, .fade-leave-active {
+    transition: all .3s ease;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1 class="text-center"><b>SIMU CARDS</b><small class="text-muted"> A Harvey&trade; Product</small></h1>
+            <nav class="navbar navbar-expand-lg navbar-dark secondary-color lighten-1">
+                <a class="navbar-brand" href="#"><img src="../assets/logo.png" width="70" height="60" alt=""></a>
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" @click="pageSelected='cardFront'">Front
+                    <span class="sr-only"></span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" @click="pageSelected='cardInsideLeft'">Inside Left</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" @click="pageSelected='cardInsideRight'">Inside Right</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" @click="pageSelected='cardBack'">Back</a>
+                </li>
+                </ul>
+            </nav>
+    </div>    
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            pageSelected: ''
+        }
+    },
+    watch: {
+        pageSelected: function () {
+            this.$emit('pageWasChanged', this.pageSelected)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        margin-top: 10px;
+    }
+</style>
