c8f2d646df351f14e8e5c327d189433276249d0d
dez.osk@gmail.com <dez.osk@gmail.com>
2020-02-22 00:31:04 +0000

    Adding repeatable components

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -1,17 +1,34 @@
 <template>
     <div class="row">
         <div class="col-sm-6 card edit-area">
-
+            <sc-text-input @displayTextChanged="textBoxValue1=$event"></sc-text-input>
+            <sc-text-input @displayTextChanged="textBoxValue2=$event"></sc-text-input>
+            <sc-text-input @displayTextChanged="textBoxValue3=$event"></sc-text-input>
         </div>
         <div class="col-sm-6 card card-display">
-
+            <sc-text-output :displayText="textBoxValue1"></sc-text-output>
+            <sc-text-output :displayText="textBoxValue2"></sc-text-output>
+            <sc-text-output :displayText="textBoxValue3"></sc-text-output>
         </div>
     </div> 
 </template>
  
 <script>
+import TextInput from './TextInput.vue'
+import TextOutput from './TextOutput.vue'
+
 export default {
-    
+    data: () => {
+      return {
+          textBoxValue1: '',
+          textBoxValue2: '',
+          textBoxValue3: ''
+      }  
+    },
+    components: {
+        scTextInput: TextInput,
+        scTextOutput: TextOutput
+    }    
 }
 </script>
 
---
+++ src/components/card/TextInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="md-form">
+                <label for="textareainput"><h4>Edit Text:</h4></label><br /><br />
+                <textarea id="textareainput" class="form-control rounded-0" rows="3" 
+                placeholder="Add text here"
+                v-model="textBoxInput"
+                @keyup="textChanged"></textarea>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            textBoxInput: ""
+        }
+    },
+    methods: {
+        textChanged: function () {
+            this.$emit('displayTextChanged', this.textBoxInput)
+        }
+    }
+}
+</script>
